73db18b6b1138c665af8026312f272019d81731b
Pradeep <webreinvent@gmail.com>
2020-02-11 02:52:37 +0530

    ci

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,7 @@
 #!/bin/sh -l
 
-if [ "$ACTION" = 'push' ]
-then
-echo "running following command"
-echo "git ftp push"
-git ftp push \
+git ftp $ACTION \
   --auto-init \
   --user $INPUT_USER \
   --passwd $INPUT_PASSWORD \
   $INPUT_URL
-fi
-
-if [ "$ACTION" = 'catchup' ]
-then
-echo "running following command"
-echo "git ftp catup"
-git ftp catchup \
-  --auto-init \
-  --user $INPUT_USER \
-  --passwd $INPUT_PASSWORD \
-  $INPUT_URL
-fi
