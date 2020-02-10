df2c94e213c98d2bd7a3e8169056ac076fa87d9f
Pradeep <webreinvent@gmail.com>
2020-02-11 02:50:00 +0530

    if test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 if [ "$ACTION" = 'push' ]
 then
+echo "running following command"
+echo "git ftp push"
 git ftp push \
   --auto-init \
   --user $INPUT_USER \
@@ -11,6 +13,8 @@ fi
 
 if [ "$ACTION" = 'catchup' ]
 then
+echo "running following command"
+echo "git ftp catup"
 git ftp catchup \
   --auto-init \
   --user $INPUT_USER \
