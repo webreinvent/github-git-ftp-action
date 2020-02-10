c586f9e92f1fe4df1ea6069dc7fa1527b04b8f69
Pradeep <webreinvent@gmail.com>
2020-02-11 02:19:02 +0530

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/sh -l
 
-
-echo $INPUT_USER
-echo $INPUT_PASSWORD
-echo $INPUT_URL
-
 git ftp push \
   --auto-init \
-  -vv \
   --user $INPUT_USER \
   --passwd $INPUT_PASSWORD \
   $INPUT_URL
